94482d67d1bbf626c86bbac804a9dff480c9e7a4
FauziAkram <fauzi.dabat@hotmail.com>
2025-06-11 20:25:20 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -192,7 +192,7 @@ void init_attack_tables() {
             ((b << 17) & ~0x0101010101010101ULL) | ((b << 15) & ~0x8080808080808080ULL) |
             ((b << 10) & ~0x0303030303030303ULL) | ((b << 6)  & ~0xC0C0C0C0C0C0C0C0ULL) |
             ((b >> 17) & ~0x8080808080808080ULL) | ((b >> 15) & ~0x0101010101010101ULL) |
-            ((b >> 10) & ~0xC0C0C0C0C0C0C0C0ULL) | ((b >> 6)  & ~0x0303030303030303ULL)
+            ((b >> 10) & ~0xC0C0C0C0C0C0C0C0ULL) | ((b >> 6)  & ~0x03030303030303ULL)
         );
 
         king_attacks_bb[sq] = north(b) | south(b) | east(b) | west(b) |
@@ -1342,7 +1342,7 @@ void uci_loop() {
                 init_tt(g_configured_tt_size_mb);
                 g_tt_is_initialized = true;
             }
-            
+
             // Check for single legal move case
             std::vector<Move> root_pseudo_moves;
             generate_moves(uci_root_pos, root_pseudo_moves);
@@ -1397,7 +1397,7 @@ void uci_loop() {
                     } else {
                         base_time_slice = my_time / 25; // Default: aim for ~25 moves
                     }
-                    search_budget_ms = base_time_slice + my_inc - 50; // Subtract margin
+                    search_budget_ms = base_time_slice + (long long)(my_inc * 0.8) - 50; // Use most of increment, subtract margin
                     // Don't use too much time if total time is low or a lot of time if total time is high
                     if (my_time > 100 && search_budget_ms > my_time * 0.8) search_budget_ms = (long long)(my_time * 0.8);
                 } else {
