e6074dc55280f19b6e568174071e5aeda8137733
FauziAkram <fauzi.dabat@hotmail.com>
2025-07-29 19:17:52 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1889,7 +1889,7 @@ void uci_loop() {
         ss >> token;
 
         if (token == "uci") {
-            std::cout << "id name Amira 1.43\n";
+            std::cout << "id name Amira 1.44\n";
             std::cout << "id author ChessTubeTree\n";
             std::cout << "option name Hash type spin default " << TT_SIZE_MB_DEFAULT << " min 0 max 1024\n";
             std::cout << "uciok\n" << std::flush;
